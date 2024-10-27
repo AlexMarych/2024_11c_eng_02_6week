e6623e3482aebc2bd49712499b1c9dfb67a38b5b
s28925 <s28925@pjwstk.edu.pl>
2024-10-27 18:51:07 +0100

    Weapon projectile firing force fix

--- Assets/Scripts/Weapon/SpawnProjectileWithVelocity.cs
+++ Assets/Scripts/Weapon/SpawnProjectileWithVelocity.cs
@@ -4,11 +4,11 @@ public class SpawnProjectileWithVelocity : MonoBehaviour
 {
     [SerializeField] private Transform _projectileOrigin;
     [SerializeField] private GameObject _projectile;
-    [SerializeField] private float Force;
+    [SerializeField] private float _force;
 
     public void Spawn(Vector3 direction)
     {
         GameObject projectile = Instantiate(_projectile, _projectileOrigin.position, Quaternion.identity);
-        projectile.GetComponent<Rigidbody2D>().velocity = (Vector2) direction.normalized * Force;
+        projectile.GetComponent<Rigidbody2D>().velocity = ((Vector2) direction).normalized * _force;
     }
 }
