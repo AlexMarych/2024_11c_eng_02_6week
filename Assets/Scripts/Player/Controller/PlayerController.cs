b03a1cca7893e2def10d98528c342818efe7d225
koliia <s28339@pjwstk.edu.pl>
2024-11-01 01:16:30 +0100

    [Material][Controller] Material update

--- Assets/Scripts/Player/Controller/PlayerController.cs
+++ Assets/Scripts/Player/Controller/PlayerController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,18 +5,26 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private CapsuleCollider2D col;
     private FrameInput frameInput;
+    private Vector2 frameVelocity;
 
     [Range(0.01f, 0.99f)]
     public float HorizontalDeadZoneThreshold = 0.1f; //Minimum input required before recognized;
     [Range(0f, 0.5f)]
     public float GrounderDistance = 0.05f; //Distance for grounded check raycast;
+    public float JumpPower = 36; //velocity of the jump;
+    [Tooltip("The maximum vertical movement speed")]
+    public float MaxFallSpeed = 40; //max falling speed
+    public float FallAcceleration = 110; //player's falling acceleration to the maximum speed
+    
+    
     public LayerMask PlayerLayer;
+    private bool cachedQueriesStartInColliders;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
-        
+        cachedQueriesStartInColliders = Physics2D.queriesStartInColliders;
     }
 
     private void Update()
@@ -38,32 +43,81 @@ public class PlayerController : MonoBehaviour
         };
 
         frameInput.Move.x = Mathf.Abs(frameInput.Move.x) < HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(frameInput.Move.x);
+
+        if (frameInput.JumpDown)
+        {
+            jumpToConsume = true;
+        }
     }
 
     private void FixedUpdate()
     {
         GroundCheck();
 
-        // HandleJump();
+        HandleJump();
         // HandleDirection();
-        // HandleGravity();
+        HandleGravity();
         //
-        // ApplyMovement();
+        ApplyMovement();
     }
 
     #region GroundCheck
-
-    private float frameNotGrounded = float.MinValue;
+    
     private bool grounded;
 
     private void GroundCheck()
     {
         Physics2D.queriesStartInColliders = false;
 
-        bool groundHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.down, GrounderDistance, PlayerLayer);    
+        bool groundHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.down, GrounderDistance, PlayerLayer);
+
+        if (!grounded && groundHit)
+        {
+            grounded = true;
+        }
+        else if (grounded && !groundHit)
+        {
+            grounded = false;
+        }
+
+        Physics2D.queriesStartInColliders = cachedQueriesStartInColliders;
     }
     
     #endregion
+
+    #region Jump
+
+    private bool jumpToConsume;
+
+    private void HandleJump()
+    {
+        if(!jumpToConsume) return;
+        
+        if (grounded)
+        {
+            ExecuteJump();
+        }
+        
+        jumpToConsume = false;
+    }
+
+    private void ExecuteJump()
+    {
+        frameVelocity.y = JumpPower;
+    }
+
+    #endregion
+
+    #region Gravity
+
+    private void HandleGravity()
+    {
+        frameVelocity.y = Mathf.MoveTowards(frameVelocity.y, -MaxFallSpeed, FallAcceleration * Time.fixedDeltaTime);
+    }
+
+    #endregion
+    
+    private void ApplyMovement() => rb.velocity = frameVelocity;
 
 
     private struct FrameInput
