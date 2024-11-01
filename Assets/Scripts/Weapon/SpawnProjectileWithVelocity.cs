3dad28e9895f1ecfa93a87648a0f03c4c5f749d6
s27743 <s27743@pjwstk.edu.pl>
2024-11-01 14:31:03 +0100

    Cannon shot vfx

--- Assets/Scripts/Weapon/SpawnProjectileWithVelocity.cs
+++ Assets/Scripts/Weapon/SpawnProjectileWithVelocity.cs
@@ -4,11 +4,17 @@ public class SpawnProjectileWithVelocity : MonoBehaviour
 {
     [SerializeField] private Transform _projectileOrigin;
     [SerializeField] private GameObject _projectile;
+    [SerializeField] private GameObject _particles;
     [SerializeField] private float _force;
 
     public void Spawn(Vector3 direction)
     {
-        GameObject projectile = Instantiate(_projectile, _projectileOrigin.position, Quaternion.identity);
-        projectile.GetComponent<Rigidbody2D>().velocity = ((Vector2) direction).normalized * _force;
+        var normalizedDirection = ((Vector2)direction).normalized;
+        Instantiate(_particles, 
+            _projectileOrigin.position, 
+            Quaternion.AngleAxis(Mathf.Atan2(normalizedDirection.y, normalizedDirection.x) * Mathf.Rad2Deg, Vector3.forward));
+
+        var projectile = Instantiate(_projectile, _projectileOrigin.position, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = normalizedDirection * _force;
     }
 }
