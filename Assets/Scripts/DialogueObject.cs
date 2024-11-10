76393afbdd60fc7c3e47ae656356d71e7aab94fd
scss28 <83139747+scss28@users.noreply.github.com>
2024-11-10 13:59:29 +0100

    Removed unused files.

--- Assets/Scripts/DialogueObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DialogueObject : ScriptableObject
-{
-	public string Name;
-	public List<string> Paragraphs;
-}
